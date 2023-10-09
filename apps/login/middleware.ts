30c25553ac61d201db484020188a2869de0946fc
peintnermax <max@caos.ch>
2023-10-09 13:23:06 +0200

    log nexturl request

--- apps/login/middleware.ts
+++ apps/login/middleware.ts
@@ -12,6 +12,7 @@ export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-zitadel-login-client", SERVICE_USER_ID);
 
+  console.log(JSON.stringify(request.nextUrl));
   requestHeaders.set("Forwarded", `host="${request.nextUrl.host}"`);
 
   const responseHeaders = new Headers();
