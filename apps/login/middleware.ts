860024234f9c80ac7fddebf74c9600e6a6630ded
peintnermax <max@caos.ch>
2024-02-09 14:33:43 +0100

    rm forwarded, set x-zitadel-forwarded

--- apps/login/middleware.ts
+++ apps/login/middleware.ts
@@ -9,14 +9,12 @@ const INSTANCE = process.env.ZITADEL_API_URL;
 const SERVICE_USER_ID = process.env.ZITADEL_SERVICE_USER_ID as string;
 
 export function middleware(request: NextRequest) {
-  const requestHeaders = new Headers({});
+  const requestHeaders = new Headers();
   requestHeaders.set("x-zitadel-login-client", SERVICE_USER_ID);
 
-  const host = request.nextUrl.host;
-  requestHeaders.set("x-zitadel-forwarded", `host="${host}"`);
+  requestHeaders.set("x-zitadel-forwarded", `host="${request.nextUrl.host}"`);
+  requestHeaders.delete("forwarded");
 
-  console.log(`host="${host}"`);
-  console.log(requestHeaders);
   const responseHeaders = new Headers();
   responseHeaders.set("Access-Control-Allow-Origin", "*");
   responseHeaders.set("Access-Control-Allow-Headers", "*");
