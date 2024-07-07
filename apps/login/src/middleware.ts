5fe2a0b7a4caba94df38cb8c5fd9a02171752626
peintnermax <max@caos.ch>
2024-07-07 15:13:41 +0200

    do not proxy all .well-knowns

--- apps/login/src/middleware.ts
+++ apps/login/src/middleware.ts
@@ -2,7 +2,11 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export const config = {
-  matcher: ["/.well-known/:path*", "/oauth/:path*", "/oidc/:path*"],
+  matcher: [
+    "/.well-known/openid-configuration",
+    "/oauth/:path*",
+    "/oidc/:path*",
+  ],
 };
 
 const INSTANCE = process.env.ZITADEL_API_URL;
