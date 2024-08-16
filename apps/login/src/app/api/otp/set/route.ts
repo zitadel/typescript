de1e50ba8690abe318b83eea00ca9750a57ab8b7
peintnermax <max@caos.ch>
2024-08-16 16:56:31 +0200

    host

--- apps/login/src/app/api/otp/set/route.ts
+++ apps/login/src/app/api/otp/set/route.ts
@@ -48,6 +48,7 @@ export async function POST(request: NextRequest) {
         }
 
         return setSessionAndUpdateCookie(
+          request.nextUrl.host,
           recent,
           checks,
           undefined,
