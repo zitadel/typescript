af8851fc14765f2d3b1f7a3e3e6dffb8200e3c6a
peintnermax <max@caos.ch>
2024-08-16 16:58:59 +0200

    server action host

--- apps/login/src/lib/server-actions.ts
+++ apps/login/src/lib/server-actions.ts
@@ -2,18 +2,24 @@
 
 import { loadMostRecentSession } from "@zitadel/next";
 import { sessionService, verifyTOTPRegistration } from "./zitadel";
+import { headers } from "next/headers";
 
 export async function verifyTOTP(
   code: string,
   loginName?: string,
   organization?: string,
 ) {
-  return loadMostRecentSession(sessionService, {
+  const host = headers().get("host");
+  if (!host) {
+    throw new Error("No host header found!");
+  }
+
+  return loadMostRecentSession(sessionService(host), {
     loginName,
     organization,
   }).then((session) => {
     if (session?.factors?.user?.id) {
-      return verifyTOTPRegistration(code, session.factors.user.id);
+      return verifyTOTPRegistration(host, code, session.factors.user.id);
     } else {
       throw Error("No user id found in session.");
     }
