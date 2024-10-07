e558ec671f110cc72ff1b5aa3a3b84d71fa47960
peintnermax <max@caos.ch>
2024-10-07 14:25:16 +0200

    middlware

--- apps/login/src/middleware.ts
+++ apps/login/src/middleware.ts
@@ -1,5 +1,6 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { getApiConfiguration } from "./lib/api";
 
 export const config = {
   matcher: [
@@ -10,10 +11,18 @@ export const config = {
   ],
 };
 
-const INSTANCE = process.env.ZITADEL_API_URL;
-const SERVICE_USER_ID = process.env.ZITADEL_SERVICE_USER_ID as string;
-
 export function middleware(request: NextRequest) {
+  const apiConfig = getApiConfiguration(request.nextUrl.host);
+
+  const INSTANCE_URL = apiConfig.url;
+  const INSTANCE_HOST = `${INSTANCE_URL}`.replace("https://", "");
+  const SERVICE_USER_ID = apiConfig.userId;
+
+  // escape proxy if the target is the same
+  if (INSTANCE_HOST === request.nextUrl.host) {
+    return NextResponse.next();
+  }
+
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-zitadel-login-client", SERVICE_USER_ID);
 
@@ -22,16 +31,13 @@ export function middleware(request: NextRequest) {
   requestHeaders.set("x-zitadel-public-host", `${request.nextUrl.host}`);
 
   // this is a workaround for the next.js server not forwarding the host header
-  requestHeaders.set(
-    "x-zitadel-instance-host",
-    `${INSTANCE}`.replace("https://", ""),
-  );
+  requestHeaders.set("x-zitadel-instance-host", INSTANCE_HOST);
 
   const responseHeaders = new Headers();
   responseHeaders.set("Access-Control-Allow-Origin", "*");
   responseHeaders.set("Access-Control-Allow-Headers", "*");
 
-  request.nextUrl.href = `${INSTANCE}${request.nextUrl.pathname}${request.nextUrl.search}`;
+  request.nextUrl.href = `${INSTANCE_URL}${request.nextUrl.pathname}${request.nextUrl.search}`;
   return NextResponse.rewrite(request.nextUrl, {
     request: {
       headers: requestHeaders,
