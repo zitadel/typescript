adc36f86e7cda38da353a9f8386c57bab4f29748
Yordis Prieto <yordis.prieto@gmail.com>
2024-12-27 11:53:46 -0500

    chore: improve idp integration using server action

--- apps/login/src/lib/server/idp.ts
+++ apps/login/src/lib/server/idp.ts
@@ -6,9 +6,43 @@ import {
   startIdentityProviderFlow,
 } from "@/lib/zitadel";
 import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 import { getNextUrl } from "../client";
 import { checkEmailVerification } from "../verify-helper";
 import { createSessionForIdpAndUpdateCookie } from "./cookie";
+
+export type RedictToIdpState = { error?: string | null } | undefined;
+
+export async function redictToIdp(
+  prevState: RedictToIdpState,
+  formData: FormData,
+): Promise<RedictToIdpState> {
+  const params = new URLSearchParams();
+
+  const linkOnly = formData.get("linkOnly") === "true";
+  const authRequestId = formData.get("authRequestId") as string;
+  const organization = formData.get("organization") as string;
+  const idpId = formData.get("id") as string;
+  const provider = formData.get("provider") as string;
+
+  if (linkOnly) params.set("link", "true");
+  if (authRequestId) params.set("authRequestId", authRequestId);
+  if (organization) params.set("organization", organization);
+
+  const response = await startIDPFlow({
+    idpId,
+    successUrl: `/idp/${provider}/success?` + params.toString(),
+    failureUrl: `/idp/${provider}/failure?` + params.toString(),
+  });
+
+  if (response && "error" in response && response?.error) {
+    return { error: response.error };
+  }
+
+  if (response && "redirect" in response && response?.redirect) {
+    redirect(response.redirect);
+  }
+}
 
 export type StartIDPFlowCommand = {
   idpId: string;
